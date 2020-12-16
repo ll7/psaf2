6f7c5e42a3b2df5806ec69bd4eba62bb5a87fd94
Valentin Höpfner <va.hoepfner@gmail.com>
2020-12-16 17:32:04 +0100

    create files

---
+++ Acting/steering_controllers/src/steering_controllers/pid_control.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import io
+import rospy
+import time
+from carla_msgs.msg import CarlaWorldInfo
+from custom_carla_msgs.msg import LaneletMap
+
+
+
+class LaneletMapProviderTest:
+    """
+    Check if the right thing was published by the lanelet_map_provider.
+    Wirtes two files in .ros: "published_test_map.osm" and "org_test_map.osm".
+    If files are the same excpet for the ids the everything worked fine.
+    You can check the results graphically in josm-editor.
+    """
+    def __init__(self, role_name):
+        self.role_name = role_name
+        self.lanelet_sub = rospy.Subscriber("/psaf/lanelet_map", LaneletMap, self.map_recieved)
+        self.osm_sub = rospy.Subscriber("/carla/world_info", CarlaWorldInfo, self.load_opendrive)
+
+        self.org_lanelet_map = None
+        self.published_lanelet_map = None
+
+
+    
+if __name__ == "__main__":
+    rospy.init_node('carla_manual_control', anonymous=True)
+    role_name = rospy.get_param("~role_name", "ego_vehicle")
+
+    lmp = LaneletMapProviderTest(role_name)
+    
+    # idle so topic is still present
+    # (resp. if world changes)
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
