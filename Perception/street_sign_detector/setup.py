de588a15ff125ef91ac1e8f1d17ce57fc49e59ba
faeibson <fbrain@gmx.com>
2021-01-20 05:32:49 +0100

    Renamed street-sign-detector to street_sign_detector

--- Perception/street_sign_detector/setup.py
+++ Perception/street_sign_detector/setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['street-sign-detector'],
+    packages=['street_sign_detector'],
     package_dir={'': 'src'}
 )
 
