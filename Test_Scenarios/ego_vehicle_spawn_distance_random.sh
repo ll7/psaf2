9d9bceeb0c68b07b4ed117acea5e62997c066296
Wolle <wut17211@gmail.com>
2021-02-18 17:38:10 +0100

    filter pointcloud

--- Test_Scenarios/ego_vehicle_spawn_distance_random.sh
+++ Test_Scenarios/ego_vehicle_spawn_distance_random.sh
@@ -5,11 +5,15 @@ sleep 5s
 gnome-terminal --execute bash -c "rviz"
 sleep 2s
 <<<<<<< HEAD
+<<<<<<< HEAD
 gnome-terminal --execute bash -c "python scenario_all_random.py -n 200"
 sleep 1s
 gnome-terminal --execute bash -c "rostopic pub /carla/ego_vehicle/target_speed std_msgs/Float64 60.0"
 =======
 gnome-terminal --execute bash -c "python scenario_all_random.py -n 20"
+=======
+gnome-terminal --execute bash -c "python scenario_all_random.py -n 200"
+>>>>>>> filter pointcloud
 sleep 1s
 gnome-terminal --execute bash -c "rostopic pub /carla/ego_vehicle/target_speed std_msgs/Float64 50.0"
 >>>>>>> combined distance and speed_limit
