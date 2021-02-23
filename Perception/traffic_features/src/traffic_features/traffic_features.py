acc94ddf0bf7fe6b1781fa0a34585acfd4acdc3a
Julius Brandl <julius.brandl97@gmail.com>
2021-02-23 20:32:31 +0100

    update perception of distance to intersection

--- Perception/traffic_features/src/traffic_features/traffic_features.py
+++ Perception/traffic_features/src/traffic_features/traffic_features.py
@@ -5,8 +5,10 @@ import numpy as np
 
 from std_msgs.msg import Float64, String
 from nav_msgs.msg import Odometry
+from custom_carla_msgs.msg import GlobalPathLanelets
 
 from commonroad.common.file_reader import CommonRoadFileReader
+
 
 class TrafficFeatures:
     def __init__(self, role_name):
@@ -14,33 +16,52 @@ class TrafficFeatures:
         self.scenario = None
         self.planning_problem_set = None
         self.current_pos = np.zeros(2)
-
+        self.lanelet_ids = []
+        self.lanelet_lengths = {}
+        self.last_non_intersection_lanelet_id = None
         
-        self.distance_pub = rospy.Publisher(f"/psaf/{self.role_name}/distance", Float64, queue_size=1)
+        self.distance_pub = rospy.Publisher(f"/psaf/{self.role_name}/distance_next_intersection", Float64, queue_size=1)
         self.map_sub = rospy.Subscriber(f"/psaf/{self.role_name}/commonroad_map", String, self.map_received)
         self.odometry_sub = rospy.Subscriber(f"carla/{self.role_name}/odometry", Odometry, self.odometry_received)
-
+        self.lanelet_sub = rospy.Subscriber(f"/psaf/{self.role_name}/global_path_lanelets", GlobalPathLanelets, self.lanelets_received)
 
     def odometry_received(self, msg):
         self.current_pos = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y])
 
-
     def map_received(self, msg):
         self.scenario, self.planning_problem_set = CommonRoadFileReader(msg.data).open()
         self.scenario.scenario_id = "DEU"
+        if self.scenario and self.lanelet_ids:
+            self.update_lanelet_lengths()
 
-    
+    def lanelets_received(self, msg):
+        self.lanelet_ids = msg.lanelet_ids + msg.adjacent_lanelet_ids
+        if self.scenario and self.lanelet_ids:
+            self.update_lanelet_lengths()
+
+    def update_lanelet_lengths(self):
+        for lanelet_id in self.lanelet_ids:
+            lanelet = self.scenario.lanelet_network.find_lanelet_by_id(lanelet_id)
+            self.lanelet_lengths[lanelet_id] = lanelet.distance
+
     def update_distance(self):
-        id = self.scenario.lanelet_network.find_lanelet_by_position([self.current_pos])
-        if len(id) == 0:
-            print("Wir haben ein Problem")
-            distance = 0
-        else:
-            lane = self.scenario.lanelet_network.find_lanelet_by_id(id[0][0])
-            last_point = lane._center_vertices[-1]
-            distance = np.sqrt((self.current_pos[0] - last_point[0])**2 + (self.current_pos[1] - last_point[1])**2)
-        self.distance_pub.publish(distance)      
-
+        possible_ids = self.scenario.lanelet_network.find_lanelet_by_position([self.current_pos])
+        if len(possible_ids) == 0:
+            print("Car is not on street. Abort.")
+            return
+        for lanelet_id in possible_ids[0]:
+            if lanelet_id in self.lanelet_ids:
+                if self.last_non_intersection_lanelet_id == self.lanelet_ids[self.lanelet_ids.index(lanelet_id) - 1]:
+                    distance = np.inf
+                else:
+                    self.last_non_intersection_lanelet_id = lanelet_id
+                    lane = self.scenario.lanelet_network.find_lanelet_by_id(lanelet_id)
+                    distances_to_center_vertices = np.linalg.norm(lane.center_vertices - self.current_pos, axis=1)
+                    idx = np.argmin(distances_to_center_vertices)
+                    distance = self.lanelet_lengths[lanelet_id][-1] - self.lanelet_lengths[lanelet_id][idx]
+                print(distance)
+                self.distance_pub.publish(distance)
+                return
 
     def run(self):
         """
