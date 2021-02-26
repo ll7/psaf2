b02ae0aeac2940930d15447415757ac2e28b0b12
Wolle <wut17211@gmail.com>
2021-02-26 17:12:58 +0100

    small changes on test_scenarios

--- Test_Scenarios/ego_vehicle_spawn_overtaking.sh
+++ Test_Scenarios/ego_vehicle_spawn_overtaking.sh
@@ -1,11 +1,11 @@
 gnome-terminal --execute bash -c "~/carla0.9.10.1/CarlaUE4.sh -opengl; bash"
 sleep 5s
-gnome-terminal --execute bash -c "source ~/carla-ros-bridge/catkin_ws/devel/setup.bash; roslaunch ego_vehicle ego_vehicle.launch spawn_point:='-20.0, 207.0, 0.0, 0.0, 0.0, 0.0'; bash"
+gnome-terminal --execute bash -c "source ~/carla-ros-bridge/catkin_ws/devel/setup.bash; roslaunch ego_vehicle ego_vehicle.launch spawn_point:='-20.0, 207.0, 0.0, 0.0, 0.0, 0.0'"
 sleep 3s 
-gnome-terminal --execute bash -c "rviz; bash"
+gnome-terminal --execute bash -c "rviz"
 sleep 2s
-gnome-terminal --execute bash -c "python scenario_overtaking.py; bash"
-sleep 15s
-gnome-terminal --execute bash -c "rostopic pub /carla/ego_vehicle/target_speed std_msgs/Float64 0.0; bash"
+gnome-terminal --execute bash -c "python scenario_overtaking.py"
+sleep 8s
+gnome-terminal --execute bash -c "rostopic pub /carla/ego_vehicle/target_speed std_msgs/Float64 60.0"
 
 
