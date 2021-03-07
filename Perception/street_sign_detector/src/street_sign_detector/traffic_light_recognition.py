e224e274a06da6c1a2019272d6eb67eff7dbe62e
YuliaK <molimoliloliloli@gmail.com>
2021-03-07 11:47:47 +0100

    traffic_light_recignition

--- Perception/street_sign_detector/src/street_sign_detector/traffic_light_recognition.py
+++ Perception/street_sign_detector/src/street_sign_detector/traffic_light_recognition.py
@@ -4,79 +4,55 @@ import numpy as np
 def detect_traffic_light_color(img):
 
     ## convert to hsv
-    hsv = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2HSV)
+    frame_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # Define lower and uppper limits of what we call "gray"
-    gray_lo = np.array([0, 0, 0])
-    gray_hi = np.array([220, 220, 220])
-    # Mask image to only select gray
-    mask_gray = cv2.inRange(hsv, gray_lo, gray_hi)
-    # Change image to red where we found grey
-    img[mask_gray > 0] = (0, 0, 0)
+    # red represents two regions in the HSV space
+    lower_red = np.array([0, 85, 110], dtype="uint8")
+    upper_red = np.array([15, 255, 255], dtype="uint8")
 
-    # Define lower and uppper limits of what we call "brown"
-    brown_lo = np.array([10, 0, 0])
-    brown_hi = np.array([20, 255, 255])
-    # Mask image to only select browns
-    mask_brown = cv2.inRange(hsv, brown_lo, brown_hi)
-    # Change image to red where we found brown
-    img[mask_brown > 0] = (0, 0, 0)
+    # red in the range of purple hue
+    lower_violet = np.array([165, 85, 110], dtype="uint8")
+    upper_violet = np.array([180, 255, 255], dtype="uint8")
 
-    # change everything to black where pixel is white
-    white_lo = np.array([0, 0, 168])
-    white_hi = np.array([172, 111, 255])
-    mask_white = cv2.inRange(hsv, white_lo, white_hi)
-    img[mask_white > 0] = (0, 0, 0)
+    #red mask
+    red_mask_orange = cv2.inRange(frame_hsv, lower_red, upper_red)  
+    red_mask_violet = cv2.inRange(frame_hsv, lower_violet, upper_violet) 
 
-################# find green ################
-    rows, cols, _ = img.shape
-    color_B = 0  # blue
-    color_G = 0  # green
-    color_R = 0  # red
-    color_N = 0  # neutral/gray color
+    red_mask_full = red_mask_orange + red_mask_violet 
 
-    for i in range(rows):
-        for j in range(cols):
-            k = img[i, j]
-            if k[0] > k[1] and k[0] > k[2]:
-                color_B = color_B + 1
-                continue
-            if k[1] > k[0] and k[1] > k[2]:
-                color_G = color_G + 1
-                continue
-            if k[2] > k[0] and k[2] > k[1]:
-                color_R = color_R + 1
-                continue
-            color_N = color_N + 1
-    pix_total = rows * cols
-    b = color_B / pix_total
-    g = color_G / pix_total
-    r = color_R / pix_total
-    n = color_N / pix_total
+    # green
+    lower_green = np.array([40, 85, 110], dtype="uint8")
+    upper_green = np.array([91, 255, 255], dtype="uint8")
+    green_mask = cv2.inRange(frame_hsv, lower_green, upper_green)
 
-    avg_color_per_row = np.average(img, axis=0)
-    avg_color = np.average(avg_color_per_row, axis=0)
+
+    # yellow
+    lower_yellow = np.array([22, 93, 0], dtype="uint8")
+    upper_yellow = np.array([45, 255, 255], dtype="uint8")
+    yellow_mask = cv2.inRange(frame_hsv, lower_yellow, upper_yellow)
+
+    output_img = frame_hsv.copy()
+    output_img[np.where(red_mask_full == 0) and np.where(green_mask == 0) and np.where(yellow_mask == 0)] = 0
+    output_img[np.where(yellow_mask > 0)] = [255, 0, 0]
+    output_img[np.where(green_mask > 0)] = [0, 255, 0]
+    output_img[np.where(red_mask_full > 0)] = [0, 0, 255]
+    #print(np.average(output_img))
+    average_pixel = np.average(output_img, axis=(0, 1))
 
     traffic_light_color = ''
-    
-    if (g > r) & (g > b):
-#        print("Traffic Light GREEN")
-#        print("Traffic Light GREEN")
+  
+    if (average_pixel[0]>average_pixel[1]) and (average_pixel[0]>average_pixel[2]):
+        traffic_light_color = 'red'
+    elif (average_pixel[2]>average_pixel[0]) and (average_pixel[2]>average_pixel[1]):
+        traffic_light_color = 'yellow'
+    elif (average_pixel[1]>average_pixel[0]) and (average_pixel[1]>average_pixel[2]):
         traffic_light_color = 'green'
-
-    elif (r > g) & (r > b) & (r > 0.017) & (n < 0.99):
-        #if avg_color[0] > 2 and avg_color[1] < 12 and avg_color[2] > 11:
-        if avg_color[0] < 9 and avg_color[1] > 7 and avg_color[1] > 6:
-#            print("Traffic Light YELLOW")
-#            print("Traffic Light YELLOW")
-            traffic_light_color = 'yellow'
-        else:
-#            print("Traffic Light RED")
-#            print("Traffic Light RED")
-            traffic_light_color = 'red'
-
-#    else:
-#        print("Traffic Light not identified")
+    else:
+        #traffic_light_color = 'off' 
+        #traffic_light_color = ','.join(str(x) for x in average_pixel)
+        average_pixel_off = np.average(output_img, axis=(0, 1))
+        traffic_light_color = ','.join(str(x) for x in average_pixel_off)
 
 
     return traffic_light_color
+    
