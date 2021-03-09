0557dc528c81537d1464a85233bf41070722f7af
YuliaK <molimoliloliloli@gmail.com>
2021-03-10 00:27:25 +0100

    Update traffic_lights_intersection.py

--- Acting/traffic_lights_intersection/src/traffic_lights_intersection/traffic_lights_intersection.py
+++ Acting/traffic_lights_intersection/src/traffic_lights_intersection/traffic_lights_intersection.py
@@ -18,14 +18,14 @@ class BehaviorTrafficLights():
                                                 self.radar_info)
 
         self._target_speed_subscriber = rospy.Subscriber("/carla/{}/target_speed".format(role_name), Float64,
-                                                         self.target_speed)
+                                                         self.update_target_speed)
         #self._odometry_subscriber = rospy.Subscriber("/carla/{}/speedometer".format(role_name), Float32, self.speed)
 
         # PUBLISHERS
         # self.entry_state_pub = rospy.Publisher("/carla/{}/vehicle_entry_state_interseption".format(role_name), Int8, queue_size=1)
         self.target_speed_pub = rospy.Publisher("/carla/ego_vehicle/target_speed", Float64, queue_size=1)
 
-    def target_speed(self, speed):
+    def update_target_speed(self, speed):
         """
         callback on target speed
         """
@@ -39,39 +39,54 @@ class BehaviorTrafficLights():
         return self.distance_inter
 
     def istance_stop_line(self, distance_stop):
-        """
-        callback on distance_stop_line
-        """
-        self.distance_stop_line = distance_stop.data
-        return self.distance_stop_line
+        #"""
+        #callback on distance_stop_line
+        #"""
+        print('if there is a stop line call an external function')
 
     def traffic_light_info (self, perception):
         """
         callback on traffic light state
         """
-        speed = 5
-        print(perception.values)
-        if ('red' in perception.values) or ('yellow' in perception.values):
-        #if (perception.values == ['red']) or (light == "yellow"):
-            self.stop()
-            rospy.loginfo("STOP")
+        x_coord = 1
+        for x in perception.relative_x_coord:
+            coord = abs(0.5 - x)
+            if coord < x_coord:
+                x_coord = coord
+                light_nr = perception.relative_x_coord.index(x)
+        #if ('red' in perception.values) or ('yellow' in perception.values):
+        if len(perception.values) != 0:
+            if (perception.values[light_nr] == 'red') or (perception.values[light_nr] == "yellow"):
+                self.stop()
         else:
-            self.no_red()
-            #runn(self)
-        return speed
+            self.go()
 
-    def no_red(self):
+    def go(self):
         rospy.loginfo("RUN")
 
-    def radar_info (self, msg):
+    def vehicle_front (self, msg):
         """
          callback on vehicle front
         """
-        self.dictance_to_vehicle = msg.data
-        return self.dictance_to_vehicle
+        #self.dictance_to_vehicle = msg.data
+
+        # NUR FÜR TEST:
+        client = carla.Client(args.host, args.port)
+        world = client.load_world('Town01')
+        blueprints = world.get_blueprint_library().filter(args.filterv)
+        #get current vehicle transform to get both location & rotation
+        #tf = current_v.get_tranform()
+        #Spawn new vehicle with some distance addition to above transformation location (say 10meter in x, y)
+        new_loc = carla.Location(99, 136.4, 0)
+        #world.spawn(new_vehicle, carla.Transform(new_loc, carla.Rotation()))
+        #transform = Transform(Location(x=99, y=136.4, z=0), Rotation(yaw=180))
+        actor = world.spawn_actor(blueprint, new_loc)
+
+
+        #return self.dictance_to_vehicle
     
     def update_speed (self, speed):
-        print(speed)
+        print(' ')
 
     def stop (self):
         rospy.loginfo("STOP")
