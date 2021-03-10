376323d4577545bcde0b7e12273ba4d4b75d84e8
YuliaK <molimoliloliloli@gmail.com>
2021-03-10 08:50:34 +0100

    Update traffic_lights_intersection.py

--- Acting/traffic_lights_intersection/src/traffic_lights_intersection/traffic_lights_intersection.py
+++ Acting/traffic_lights_intersection/src/traffic_lights_intersection/traffic_lights_intersection.py
@@ -8,43 +8,28 @@ class BehaviorTrafficLights():
 
     def __init__(self, role_name):
         self.role_name = role_name
+        self.actual_speed = 0
 
         # SUBSCRIBERS
         self.intersection_sub = rospy.Subscriber(f"/psaf/{self.role_name}/distance_next_intersection", Float64,
                                                  self.next_intersection_info)
         self.traffic_light_sub = rospy.Subscriber(f"/psaf/{self.role_name}/perception_info", PerceptionInfo,
                                                   self.traffic_light_info)
-        self.radarsubscriber = rospy.Subscriber(f"psaf/{role_name}/radar/distance", Float64,
-                                                self.radar_info)
-
-        self._target_speed_subscriber = rospy.Subscriber("/carla/{}/target_speed".format(role_name), Float64,
-                                                         self.update_target_speed)
-        #self._odometry_subscriber = rospy.Subscriber("/carla/{}/speedometer".format(role_name), Float32, self.speed)
+        self.intersection_dist__sub = rospy.Subscriber(f"/psaf/{self.role_name}/distance_next_intersection", Float64,
+                                                       self.next_intersection_info)
+        #self._target_speed_subscriber = rospy.Subscriber("/carla/{}/target_speed".format(role_name), Float64,
+        #                                                 self.update_target_speed)
+        self.speed_sub = rospy.Subscriber(f"/psaf/{self.role_name}/vehicle_status", Float32,
+                                          self.speed_update)
+        #self.radar_sub = rospy.Subscriber(
+        #    f"/carla/{role_name}/radar/front/radar_points", PointCloud2, self.radar_updated)
 
         # PUBLISHERS
-        # self.entry_state_pub = rospy.Publisher("/carla/{}/vehicle_entry_state_interseption".format(role_name), Int8, queue_size=1)
+        self.entry_state_pub = rospy.Publisher("/carla/{}/vehicle_entry_state_interseption".format(role_name), Int8, queue_size=1)
         self.target_speed_pub = rospy.Publisher("/carla/ego_vehicle/target_speed", Float64, queue_size=1)
+        self.traffic_stop_pub = rospy.Publisher("/psaf/{}/traffic_stop_dist".format(role_name), Float64, queue_size=1)
 
-    def update_target_speed(self, speed):
-        """
-        callback on target speed
-        """
-        target_speed = speed.data
-
-    def next_intersection_info (self, distance_intersection):
-        """
-        callback on distance_next_intersection
-        """
-        self.distance_inter = distance_intersection.data
-        return self.distance_inter
-
-    def istance_stop_line(self, distance_stop):
-        #"""
-        #callback on distance_stop_line
-        #"""
-        print('if there is a stop line call an external function')
-
-    def traffic_light_info (self, perception):
+    def traffic_light_info(self, perception):
         """
         callback on traffic light state
         """
@@ -61,51 +46,46 @@ class BehaviorTrafficLights():
         else:
             self.go()
 
-    def go(self):
-        rospy.loginfo("RUN")
-
-    def vehicle_front (self, msg):
+    def next_intersection_info(self, intersection_dist):
         """
-         callback on vehicle front
+        callback on distance_next_intersection
         """
-        #self.dictance_to_vehicle = msg.data
+        stop_dist_intersec = intersection_dist.data
 
-        # NUR FÜR TEST:
-        client = carla.Client(args.host, args.port)
-        world = client.load_world('Town01')
-        blueprints = world.get_blueprint_library().filter(args.filterv)
-        #get current vehicle transform to get both location & rotation
-        #tf = current_v.get_tranform()
-        #Spawn new vehicle with some distance addition to above transformation location (say 10meter in x, y)
-        new_loc = carla.Location(99, 136.4, 0)
-        #world.spawn(new_vehicle, carla.Transform(new_loc, carla.Rotation()))
-        #transform = Transform(Location(x=99, y=136.4, z=0), Rotation(yaw=180))
-        actor = world.spawn_actor(blueprint, new_loc)
+        return stop_dist_intersec
 
+    def vehicle_front_info(self, msg):
+        """
+        callback on vehicle front
+        """
+        stop_dist_vehicle = 20 #test value
+        return stop_dist_vehicle
 
-        #return self.dictance_to_vehicle
-    
-    def update_speed (self, speed):
-        print(' ')
+    def speed_update (self, speed):
+        self.actual_speed = speed.velocity
 
-    def stop (self):
+    def stop(self):
         rospy.loginfo("STOP")
+        self.stop_dist_intersec = 30 # for test
+        self.stop_dist_vehicle = 50 # for test
+        self.stop_distance = min(self.stop_dist_intersec, self.stop_dist_vehicle) # maximum distance to stop
+        self.current_speed = self.actual_speed
+        braking_distances = pow((self.current_speed/10), 2)
+        if braking_distances <= self.stop_distance:
+            target_speed = 0
+            self.target_speed_pub.publish(target_speed)
+
+    def go(self):
+        """
+        callback on target speed
+        """
+        rospy.loginfo("RUN")
+        self.self.target_speed_pub.publish(target_speed)
 
     def run(self):
         """
         drive into the intersection
         """
-        #if self.traffic_light_info():
-            #if self.dictance_to_vehicle > 0:
-                #distance = self.dictance_to_vehicle
-            #elif self.distance_stop_line > 0:
-                #distance = self.distance_stop_line
-            #else:
-                #distance = self.distance_inter
-
-            #self.stop(distance)
-        #else:
-            #self.run()
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
 
@@ -125,6 +105,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
