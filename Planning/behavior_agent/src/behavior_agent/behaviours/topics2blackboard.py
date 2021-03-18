8fa5cb0f2ccc011e1a38e0cbdcdfd76fdd4b2616
Wolle <wut17211@gmail.com>
2021-03-18 17:43:37 +0100

    added lidar to behavior

--- Planning/behavior_agent/src/behavior_agent/behaviours/topics2blackboard.py
+++ Planning/behavior_agent/src/behavior_agent/behaviours/topics2blackboard.py
@@ -21,6 +21,8 @@ def create_node(role_name):
             {'name':f"/psaf/{role_name}/global_path_lanelets", 'msg':GlobalPathLanelets ,'clearing-policy': py_trees.common.ClearingPolicy.NEVER},
             {'name':f"/psaf/{role_name}/lane_status", 'msg':LaneStatus ,'clearing-policy': py_trees.common.ClearingPolicy.NEVER},
             {'name':f"/psaf/{role_name}/stopline_distance", 'msg':Float64, 'clearing-policy': py_trees.common.ClearingPolicy.NEVER},
+            {'name':f"/psaf/{role_name}/obstacle_on_left_lane", 'msg':Float64, 'clearing-policy': py_trees.common.ClearingPolicy.NEVER},
+            {'name':f"/psaf/{role_name}/obstacle_on_right_lane", 'msg':Float64, 'clearing-policy': py_trees.common.ClearingPolicy.NEVER},
          ]
     topics2blackboard = py_trees.composites.Parallel("Topics to Blackboard")
     for topic in topics:
--- Test_Scenarios/ego_vehicle_spawn_distance_random.sh
+++ Test_Scenarios/ego_vehicle_spawn_distance_random.sh
@@ -1,6 +1,6 @@
 gnome-terminal --execute bash -c "~/carla0.9.10.1/CarlaUE4.sh -opengl; bash"
 sleep 5s
-gnome-terminal --execute bash -c "source ~/carla-ros-bridge/catkin_ws/devel/setup.bash; roslaunch ego_vehicle ego_vehicle.launch spawn_point:='20.0, 207.5, 0.0, 0.0, 0.0, 0.0'"
+gnome-terminal --execute bash -c "source ~/carla-ros-bridge/catkin_ws/devel/setup.bash; roslaunch ego_vehicle ego_vehicle.launch spawn_point:='-20.0, 207.5, 0.0, 0.0, 0.0, 0.0'"
 sleep 5s 
 gnome-terminal --execute bash -c "rviz"
 sleep 2s
