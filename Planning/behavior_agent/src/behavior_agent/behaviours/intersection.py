01cb8e7ce6355ee7b8d47c1f8f9c62fc1032e340
Valentin <halentin@github.com>
2021-03-26 22:08:53 +0100

    habs nicht geschafft :/

--- Planning/behavior_agent/src/behavior_agent/behaviours/intersection.py
+++ Planning/behavior_agent/src/behavior_agent/behaviours/intersection.py
@@ -24,10 +24,65 @@ class Approach(py_trees.behaviour.Behaviour):
         self.start_time = rospy.get_time()
         self.stopline_detected = False
         self.stopline_distance = np.inf
+        self.traffic_light_detected = False
+        self.traffic_light_distance = np.inf
+        self.traffic_light_status = ''
+        self.virtual_stopline_distance = np.inf
         self.target_speed_pub.publish(30.0)
+        self.last_virtual_distance = np.inf
         rospy.loginfo("start approaching behavior")
 
     def update(self):
+        # Update Light Info
+        light_status_msg = self.blackboard.get("/psaf/ego_vehicle/traffic_light")
+        if light_status_msg is not None:
+            self.light_status = light_status_msg.color
+            rospy.loginfo(f"Light Status: {self.light_status}")
+            self.traffic_light_distance = light_status_msg.distance
+            rospy.loginfo(f"Light distance: {self.traffic_light_distance}")
+        # Update stopline Info
+        _dis = self.blackboard.get("/psaf/ego_vehicle/stopline_distance")
+        if _dis is not None:
+            self.stopline_distance = _dis.data
+            rospy.loginfo(f"Stopline distance: {self.stopline_distance}")
+        
+        # calculate virtual stopline
+        self.virtual_stopline_distance = min(self.traffic_light_distance, self.stopline_distance)
+        
+        # calculate speed needed for stopping
+        v_stop = max(5., (self.virtual_stopline_distance/30)**1.5 * 50)
+        if v_stop > 30:
+            v_stop = 30 
+        if self.virtual_stopline_distance < 3.5:
+            v_stop = 0
+        # stop when there is no or red/yellow traffic light
+        if self.traffic_light_status == '' or self.light_status == 'red' or self.traffic_light_status == 'yellow':
+            if self.virtual_stopline_distance < 1:
+                rospy.loginfo("stopping immediately")
+                self.target_speed_pub.publish(0)
+            else:
+                rospy.loginfo(f"slowing down: {v_stop}")
+                self.target_speed_pub.publish(v_stop)
+        
+        # approach slowly when traffic light is green
+        if self.traffic_light_status == 'green':
+            self.target_speed_pub.publish(30)
+        
+        # get speed
+        odo = self.blackboard.get("/carla/ego_vehicle/odometry")
+        speed = np.sqrt(odo.twist.twist.linear.x ** 2 + odo.twist.twist.linear.y ** 2 + odo.twist.twist.linear.z ** 2)*3.6
+
+        if (self.last_virtual_distance == self.virtual_stopline_distance and self.virtual_stopline_distance < 10) or self.virtual_stopline_distance < 3.5:
+            return py_trees.common.Status.SUCCESS
+        elif (self.virtual_stopline_distance == np.inf and (rospy.get_time() - self.start_time) > 15) or (rospy.get_time() - self.start_time) > 120:
+            return py_trees.common.Status.SUCCESS
+        else:
+            return py_trees.common.Status.RUNNING
+
+
+
+
+        return py_trees.common.Status.FAILURE
         light_status_msg = self.blackboard.get("/psaf/ego_vehicle/traffic_light")
         if light_status_msg is None:
             return py_trees.common.Status.SUCCESS
